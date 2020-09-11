d2bf777ee17be98159be3e4056e21a34b8393af3
sou1z <karol.golanski@lo1.sandomierz.pl>
2020-09-11 09:10:39 +0200

    dodanie plików źródłowych cpp i pythona

---
+++ cpp/hello.cpp
@@ -0,0 +1,14 @@
+/*
+ * hello.cpp
+ * wyświetlenie prostego tekstu w terminalu
+ */
+
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	std::cout << "Hello there";
+	return 0;
+}
+
---
+++ python/pole_kola.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  pole_kola.py
+
+
+def main(args):
+    r =  input("Podaj promień: ")
+    pole = r * r * 3.14
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
